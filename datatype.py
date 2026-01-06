5893ea0c047178fac2d2ba5a5c065a519eafa2a7
bhumi-pilojpara2548 <bhumipilojpara84@gmail.com>
2026-01-06 20:41:32 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,47 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x="hello worls" #str
+print(x)
+hello worls
+x=20
+print(x) #int
+20
+x=20.5 #float
+print(x)
+20.5
+x=1j #complex
+print(x)
+1j
+x=["apple","banana","cherry"] #list
+print(x)
+['apple', 'banana', 'cherry']
+x=("cherry","strawberry","lychee") #tuple
+print(x)
+('cherry', 'strawberry', 'lychee')
+x=range(7) #range
+print(x)
+range(0, 7)
+>>> x={"name" : "john","age" : "28"} #dict
+>>> print(x)
+{'name': 'john', 'age': '28'}
+>>> x={"custerdapple","cherry","lychee"} #set
+>>> print(x)
+{'lychee', 'cherry', 'custerdapple'}
+>>> x=frozenset({"apple","banana","cherry"}) #frozenset
+>>> print(x)
+frozenset({'apple', 'banana', 'cherry'})
+>>> x=True #bool
+>>> print(x)
+True
+>>> x="hello" #bytypes
+>>> print(x)
+hello
+>>> x=bytearray(5) #bytearray
+>>> print(x)
+bytearray(b'\x00\x00\x00\x00\x00')
+>>> x=memoryview(bytes(5)) #memoryview
+>>> print(x)
+<memory at 0x00000212FC953580>
+>>> x=None #Nonetype
+>>> print(x)
+None
