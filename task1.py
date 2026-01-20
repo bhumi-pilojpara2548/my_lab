8a83866ce7274c77089be18e3a1d692da9e2c5bc
bhumi-pilojpara2548 <bhumipilojpara84@gmail.com>
2026-01-20 18:29:21 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,55 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+p=2000
+r=5
+t=2
+si=(p * r * t) / 100
+print("simple interest=",si)
+simple interest= 200.0
+a=30
+b=56
+if a > b:
+    print("maximum=", a)
+else:
+    print("maximum=", b)
+
+    
+maximum= 56
+for i in range(1, 6):
+    print(i)
+
+    
+1
+2
+3
+4
+5
+s="python"
+print("length =",len(s))
+length = 6
+print("hello world")
+hello world
+>>> s="python"
+>>> print("first character =",s[0])
+first character = p
+>>> s="atmiya"
+>>> print("last character =",s[-1])
+last character = a
+>>> n=-5
+>>> if n > 0:
+...     print("positive number")
+... else:
+...     print("nagative number")
+... 
+...     
+nagative number
+>>> a=45
+>>> b=34
+>>> c=21
+>>> sum=a + b + c
+>>> print("sum =",sum)
+sum = 100
+>>> name=input("enter ypur name:")
+enter ypur name:bhumi
+>>> print("hello",name)
+hello bhumi
