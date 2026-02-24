3ef2f7e4e81d5935f6c3e12a9c9b2a70f854daa4
bhumi-pilojpara2548 <bhumipilojpara84@gmail.com>
2026-02-24 08:11:11 +0530

    Add files via upload

---
+++ foorloop.py
@@ -0,0 +1,24 @@
+for i in range (1,6):
+    print(i)
+for i in range(3):
+    print("hello")
+for i in range (1,11):
+    print(i)
+for i in range (1,21):
+    if i % 2 == 0:
+        print(i)
+for i in range (1,10):
+    if i % 2 != 0:
+        print(i)
+for i in range (1,11):
+    print("5 *",i,"=",5*i)
+#sum of number 1 to 5
+total=0
+for i in range(1,6):
+    total=total+i
+    print("sum is: ",total)
+#print list element
+numbers=[10,20,30,40]
+for n in numbers:
+    print(n)
+  
